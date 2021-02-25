6466695a57d5ab2e30311502cc4b9718825e07f1
daryn <daryn@hex50.com>
2021-02-25 13:29:48 -0800

    tut8 - vue router

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Job Details Page</h1>
+
+  <!-- 'id' has to match the route parameter in the Jobs route in index.js -->
+  <!-- these will display anything placed after <domain>/jobs/ in the URL -->
+  <!-- <p>Job ID: {{ $route.params.id }}</p>    -->
+  <p>[{{ id }}]: {{ title }}</p>    <!-- this works with the export below -->
+</template>
+
+<script>
+export default {
+  props: ['id', 'title'], // this shortcuts the data() function below:
+  // data() {
+  //   return {
+  //     id: this.$route.params.id,
+  //     name: this.$route.params.title,
+  //   }
+  // }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: {id: job.id, title: job.title }}">
+          <h3>{{ job.title }}</h3>
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                {title: 'whisker weasle', id: 1, details: 'mooses'},
+                {title: 'fuzzy pants', id: 2, details: 'mooses'},
+                {title: 'moose breath', id: 3, details: 'mooses'},
+                {title: 'pouncer toes', id: 4, details: 'mooses'},
+                {title: 'moose pants', id: 5, details: 'mooses'},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+  .job h3{
+    background: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    cursor: pointer;
+    color: #444;
+  }
+  .job h3:hover {
+    background: #ddd;
+  }
+  .job a{
+    text-decoration: none;
+  }
+</style>
